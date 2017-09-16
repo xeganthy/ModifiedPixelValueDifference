00e3f5ebc9005177663a27cb400aa98747148641
xeganthy <xeganthy@gmail.com>
2017-09-16 18:16:34 +0800

    updated, wala pang preprocessing.

--- src/ARC_Algo.java
+++ src/ARC_Algo.java
@@ -1,27 +1,77 @@
 import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import javax.imageio.ImageIO;
 
-public class ARC_Algo {
-
-	public static void main(String[] args) {
-		int rangeTable[][] = new int[1][1];
+public class ARC_Algo { 	//TODO better array to image and vice versa ((no loss dapat))
+							//TODO tests for 512 and 256 images
+	public static void main(String[] args) throws IOException {
+		int[][] rangeTable = 	{{0,8,16,32,64}, 	//lj
+							     {7,15,31,63,255}, 	//uj
+								 {0,0,0,0,0}};		//tj
 		//input
 		BufferedImage image = getImage("lena_gray.bmp");
 		MessageHelper secretMessage = new MessageHelper("test.txt");
 		
 		int[][] stegoGrid = new int[image.getHeight()][image.getWidth()];
-		stegoGrid = getImagePixelValues(image, stegoGrid); 	//[col][row]
+		stegoGrid = getImagePixelValues(image, stegoGrid); 					//[col][row]
+		embedTableClue(stegoGrid[0][0], imageClassification(stegoGrid)); 	//preprocess, true if smooth, else false
 		
-		List<Block> blocks = pixelDivision(stegoGrid);		//pixel division
-		//embedBlock(blocks.get(1), secretMessage, rangeTable);
+		List<Block> blocks = pixelDivision(stegoGrid);						//pixel division
+		rangeTable[2] = getEmbeddableBits(rangeTable);
+		int gg = 0;
 		for(int i = 0; i < blocks.size(); i++){			//embedding phase
-			embedBlock(blocks.get(i), secretMessage, rangeTable);
+			if(secretMessage.getCurrentBit() <= secretMessage.getFinalBit()){
+				embedBlock(blocks.get(i), secretMessage, rangeTable);
+				gg++;
+			}
 		}
+		updateGrid(stegoGrid, blocks);
+		createStegoImage(stegoGrid);
 		
+		//extraction
+		BufferedImage stegoImage = getImage("stegoImage.bmp");
+		int[][] embeddedStegoGrid = new int[stegoImage.getHeight()][stegoImage.getWidth()];
+		embeddedStegoGrid = getImagePixelValues(stegoImage, embeddedStegoGrid);
+		List<Block> embeddedBlocks = pixelDivision(embeddedStegoGrid);
+		
+		String embeddedSecretMessage = "";
+		for(int i = 0; i < gg; i++) { //TODO when to stop
+//			System.out.println(i);
+			embeddedSecretMessage += extractBlock(blocks.get(i), rangeTable);
+		}
+		printBlockInfo(blocks, blocks, "embeddedBlocks");
+		//testing(blocks, embeddedBlocks);
+		
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter
+				(new FileOutputStream("secMess.txt"), "utf-8"));
+		writer.write(embeddedSecretMessage);
+		writer.flush();
+		writer.close();
+		
+		System.out.println(MessageHelper.binaryToASCII("secMess.txt"));
+	}
+	
+	public static boolean imageClassification(int[][] stegoGrid) { //TODO
+		
+		
+		return true;
+	}
+	
+	public static void embedTableClue(int pixel, boolean isSmooth) {
+		if(isSmooth) {
+			//embed on pix
+		} else {
+			//embed on pix
+		}
+	}
+	
+	public static void updateGrid(int[][] stegoGrid, List<Block> blocks) {
 		int counter = 0;
 		for(int i = 0; i < stegoGrid.length; i++) { //replace each block's value to image
 			for(int j = 0; j < stegoGrid[0].length - 2; j += 3){
@@ -31,7 +81,6 @@ public class ARC_Algo {
 				counter++;
 			}
 		}
-		createStegoImage(stegoGrid);
 	}
 	public static BufferedImage getImage(String path) {
 		BufferedImage img = null;
@@ -88,20 +137,27 @@ public class ARC_Algo {
 	
 	public static void embedBlock(Block block, MessageHelper secretMessage, int[][] rangeTable) {
 		int basePixel3LSB = (byte) (block.getBasePixel() & ((1 << 3) - 1)); 
+		String toEmbed = secretMessage.peekSecretBits(3);
 		int secretMessageLSB = Integer.parseInt(secretMessage.peekSecretBits(3), 2);
+		//LSB
 		int embeddedBasePixel = replaceLeastSignificantBit
-				(block.getBasePixel(), 3, Integer.parseInt(secretMessage.getSecretBits(3), 2)); //LSB
+				(block.getBasePixel(), 2, Integer.parseInt(secretMessage.getSecretBits(3), 2)); 
+		//OPAP
 		block.setBasePixel(opapPixel(basePixel3LSB, secretMessageLSB, embeddedBasePixel));
+		
 		//DETERMINE RANGE
+		
 		block.setLeftRange(locateTableRange(Math.abs(block.getBasePixel() - block.getLeftPixel()), rangeTable));
 		block.setRightRange(locateTableRange(Math.abs(block.getBasePixel() - block.getRightPixel()), rangeTable));
-		
 		//PVD ((EMBEDDING OF SECRET MESSAGE))
+		toEmbed += secretMessage.peekSecretBits(rangeTable[2][block.getRangeIndex("left")]);
 		int embeddedLeftPixel = embedPixelPVD("left", block, secretMessage, rangeTable);
+		toEmbed += secretMessage.peekSecretBits(rangeTable[2][block.getRangeIndex("right")]);
 		int embeddedRightPixel = embedPixelPVD("right", block, secretMessage, rangeTable);
 		
 		block.setLeftPixel(embeddedLeftPixel);
 		block.setRightPixel(embeddedRightPixel);
+		block.setEmbedded(toEmbed);
 	}
 	
 	public static int embedPixelPVD(String pixel, Block block, MessageHelper secretMessage, int[][] rangeTable) {
@@ -158,8 +214,87 @@ public class ARC_Algo {
 			modifiedPixel = (byte) (modifiedPixel & ~(1 << i)) & 0xff; 			//make 3 LSB 0
 		}
 		modifiedPixel = ((byte) modifiedPixel | (byte)toEmbed)& 0xff ; 
-		
 		return modifiedPixel;
+	}
+	
+	public static int getLeastSignificantBit(int embeddedPixel, int bitsToGet) {
+		return (byte) (embeddedPixel & ((1 << bitsToGet) - 1));
+	}
+	public static int[] getEmbeddableBits(int[][] rangeTable) {
+		int[] embeddableSecretBits = new int[rangeTable[0].length];
+		for(int i = 0; i < rangeTable[0].length; i++) {
+			embeddableSecretBits[i] = (i <= 3) ? 
+					(int)Math.round((Math.log(rangeTable[1][i]-rangeTable[0][i]) / Math.log(2))): 
+					(int)Math.round((Math.log(rangeTable[0][i]) / Math.log(2)));
+			System.out.print(embeddableSecretBits[i] + " ");
+		}
+		return embeddableSecretBits;
+	}
+	
+	public static int extractPVD(String dir, int diff, int[][] rangeTable, Block block) {
+		if(dir.equals("left")) {
+			return Math.abs(diff - rangeTable[0][block.getLeftRange()]);
+		} else {
+			return Math.abs(diff - rangeTable[0][block.getRightRange()]);
+		}
+	}
+	
+	public static String extractBlock(Block block, int[][] rangeTable) {
+		//LSB Extraction
+		int basePixelGet3LSB = (byte) (block.getBasePixel() & ((1 << 3) - 1));
+		
+		int diffL = Math.abs(block.getLeftPixel() - block.getBasePixel());
+		int diffR = Math.abs(block.getRightPixel() - block.getBasePixel());
+		
+		block.setLeftRange(locateTableRange(diffL, rangeTable));
+		block.setRightRange(locateTableRange(diffR, rangeTable));
+		
+		int secretMessageLeft = extractPVD("left", diffL, rangeTable, block);
+		int secretMessageRight = extractPVD("right", diffR, rangeTable, block);
+		
+		String basePixelMessage = foo(3, Integer.toBinaryString(basePixelGet3LSB));
+		String leftPixelMessage = foo(rangeTable[2][block.getLeftRange()], Integer.toBinaryString(secretMessageLeft));
+		String rightPixelMessage = foo(rangeTable[2][block.getRightRange()], Integer.toBinaryString(secretMessageRight));
+		
+		//System.out.println(leftPixelMessage+" "+rangeTable[2][block.getLeftRange()]);
+		//System.out.println(rightPixelMessage+" "+rangeTable[2][block.getRightRange()]);
+		block.setExtracted(basePixelMessage + leftPixelMessage + rightPixelMessage);
+		return basePixelMessage + leftPixelMessage + rightPixelMessage;
+	}
+	
+	public static String foo(int size, String msg) {
+		while(msg.length() != size) {
+			msg = '0'+msg;
+		}
+		return msg;
+	}
+	
+	public static void printBlockInfo(List<Block> blocks, List<Block> embeddedBlocks, String name) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter
+				(new FileOutputStream(name+".txt"), "utf-8"));
+		for(int i = 0; i < blocks.size(); i++) {
+			writer.write("Block "+i+" "+blocks.get(i).getLeftPixel()+" "+blocks.get(i).getBasePixel()+" "+blocks.get(i).getRightPixel());
+			writer.newLine();
+			writer.write("Embeddable: L "+blocks.get(i).getLeftRange()+" R "+blocks.get(i).getRightRange());
+			writer.newLine();
+			writer.write("Embedded:  "+blocks.get(i).getEmbedded());
+			writer.newLine();
+			writer.write("Extracted: "+embeddedBlocks.get(i).getExtracted());
+			writer.newLine();
+		}
+		writer.flush();
+		writer.close();
+	}
+	
+	public static String testing(List<Block> blocks, List<Block> embeddedBlocks) {
+		int i = 1;
+		while(!blocks.get(i).getEmbedded().isEmpty() && !embeddedBlocks.get(i).getExtracted().isEmpty()) {
+			if(!blocks.get(i).getEmbedded().equals(embeddedBlocks.get(i).getExtracted())){
+				System.out.println(i);
+				i++;
+			}
+		}
+		return Integer.toString(i);
 	}
 	
 }
