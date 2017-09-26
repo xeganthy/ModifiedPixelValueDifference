d3ab2d16bfcb5f4fda8288597129a1e40551b626
ultheiyr <30291762+ultheiyr@users.noreply.github.com>
2017-09-26 21:11:13 +0800

    Metrics calculator
    tested result

--- src/MetricsCalculator.java
+++ src/MetricsCalculator.java
@@ -4,11 +4,13 @@ public class MetricsCalculator {
 	
 	public static void main(String[] args) {
 		BufferedImage originalImage = ImageHelper.getImage("/Users/MARK ANTONIO/Documents/GitHub/ModifiedPixelValueDifference/lena_gray.bmp");				//bmp image
-		int[][] origImage = new int[originalImage.getHeight()][originalImage.getWidth()];	//initialize array
+		int[][] origImage = new int[originalImage.getHeight()][originalImage.getWidth()];	
 		origImage = ImageHelper.getImagePixelValues(originalImage, origImage);
 		BufferedImage arcStegoImage = ImageHelper.getImage("/Users/MARK ANTONIO/Documents/GitHub/ModifiedPixelValueDifference/ARCStegoImage.bmp");
-		int[][] stegoImage = new int[arcStegoImage.getHeight()][arcStegoImage.getWidth()];	//initialize array
+		int[][] stegoImage = new int[arcStegoImage.getHeight()][arcStegoImage.getWidth()];	
 		stegoImage = ImageHelper.getImagePixelValues(arcStegoImage, stegoImage); 
+		
+		System.out.println(calculatePSNR(origImage, stegoImage));
 		
 	}
 	
@@ -17,11 +19,10 @@ public class MetricsCalculator {
 		for(int i = 0; i < origImage.length; i++) {
 			for(int j = 0; j < origImage[0].length; j++) {
 				mse += Math.pow(origImage[i][j]-stegoImage[i][j], 2);
-				
 			}
 		}
 		mse = 1/mse;
-		double PSNR = Math.log((Math.pow(512, 2)/mse))*10;
+		double PSNR = Math.log10((Math.pow(512, 2)/mse));
 		return PSNR;
 	}
 }
