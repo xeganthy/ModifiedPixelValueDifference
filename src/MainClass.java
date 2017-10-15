bfc5b3440ca6d58f3e2cb4b1e272b6d5308f7388
Xeganthy <renzo@xeganthy-laptop>
2017-10-16 01:33:34 +0800

    Update for inconsistencies KhodFaez bug

--- src/MainClass.java
+++ src/MainClass.java
@@ -8,13 +8,19 @@ public class MainClass {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter filename: ");
 		String fileName = sc.nextLine();
-		String imgDir = "/Users/MARK ANTONIO/Documents/GitHub/ModifiedPixelValueDifference/original_Images/"+fileName+".bmp";	//input
-		String msgDir = "/Users/MARK ANTONIO/Documents/GitHub/ModifiedPixelValueDifference/toEmbed.txt";	//input
+//		String imgDir = "/Users/MARK ANTONIO/Documents/GitHub/ModifiedPixelValueDifference/original_Images/"+fileName+".bmp";	//input
+//		String msgDir = "/Users/MARK ANTONIO/Documents/GitHub/ModifiedPixelValueDifference/toEmbed.txt";	//input
+		
+		String imgDir = "/home/renzo/git/ModifiedPixelValueDifference/original_Images/"+fileName+".bmp";
+		String msgDir = "/home/renzo/git/ModifiedPixelValueDifference/toEmbed.txt";
+		
 		BufferedImage image = ImageHelper.getImage(imgDir);							//bmp image
 		MessageHelper secretMessage = new MessageHelper("ARC");						//secretMessage ARC
 		MessageHelper secretMessage2 = new MessageHelper("KF");						//secretMessage KF ((it's the same))
 		int[][] imageGrid = new int[image.getHeight()][image.getWidth()];				//initialize array
+		int[][] imageGrid2 = new int[image.getHeight()][image.getWidth()];
 		imageGrid = ImageHelper.getImagePixelValues(image, imageGrid); 				//module 2
+		imageGrid2 = ImageHelper.getImagePixelValues(image, imageGrid2);
 		
 		secretMessage.messageToBinary(msgDir);											//module 3
 		secretMessage2.messageToBinary(msgDir);											//module 3
@@ -22,7 +28,8 @@ public class MainClass {
 		System.out.println("embedding");												//EMBEDDING
 		ARC_Algo arc = new ARC_Algo(imageGrid, secretMessage);						//Arc Algo
 		arc.embedImage();
-		KhodFaez_Algo khodFaez = new KhodFaez_Algo(imageGrid, secretMessage2);
+		
+		KhodFaez_Algo khodFaez = new KhodFaez_Algo(imageGrid2, secretMessage2);
 		khodFaez.embedImage();
 		
 //		String stgImgDirARC = "/Users/MARK ANTONIO/Documents/GitHub/ModifiedPixelValueDifference/ARCStegoImage.bmp"; 	//ARC STEGO IMG
